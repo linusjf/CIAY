d5bf5757e61e1f1e0c8e2bad2cb86095c10862ad
Linus Fernandes <linus.fernandes@gmail.com>
2025-04-13 04:37:58 +0530

    fix: let sphinx spoof user agent of browset

--- conf.py
+++ conf.py
@@ -50,3 +50,6 @@ linkcheck_ignore = [
     r'https://example\.com/redirect',  # Ignore known redirect
 ]
 linkcheck_ignore_redirects = True
+linkcheck_request_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
